6206be57abc0cb0f33690b09ed357b468a8b545c
Fus Przemyslaw <cubical97@gmail.com>
2020-05-25 18:27:39 +0200

    On master: Naprawione sprawdzenie wartości pola i wypisanie komunikatu. *Przed rozpoczęciem. *W jednym pliku.

--- Saper/Gui.py
+++ Saper/Gui.py
@@ -1,31 +1,36 @@
-from tkinter import *
+import tkinter as tk
 
 class MyGui():
     def __init__(self, okno):
         self.__okno = okno
         self.__okno.title('Saper')
-        self.__plansza_n = 8
-        self.__plansza_m = 8
-        self.__plansza_mina = 12
+        self.__plansza_n = 8      #default n = 8
+        self.__plansza_m = 8      #default m = 8
+        self.__plansza_mina = 12  #default mines = 12
+        self.__play=None
+        self.__text=tk.StringVar()
+        self.__text.set=('Wpisz wartości')
 
     def __str__(self):
-        return self.__ret
+        return 'MyGui'
 
-    def Start(self, sx=8, sy=8, smina=12):
+    def StartTry(self, sx=8, sy=8, smina=12):
         try:
             sn=int(sx)
             sm=int(sy)
             smi=int(smina)
 
             if(sn<2 or sn>15):
-                raise ValueError
-            if(sm<2 or sn>15):
-                raise ValueError
+                self.__text.set=('Wpisz wartość od 2 do 15')
+                return
+            if(sm<2 or sm>15):
+                self.__text.set=('Wpisz wartość od 2 do 15')
+                return
             if(smi<1 or smi>sm*sn-1):
-                raise ValueError
-
+                self.__text.set=('Wpisz wartość od {} do {}',format(1,str(sn*sm)))
+                return
         except ValueError as e:
-            print('Błąd\nWpisz liczby od 2 do 15!')
+            self.__text.set=('Wpisz liczby!')
         except Exception as e:
             print('Błąd\n')
             print(e.args)
@@ -34,28 +39,59 @@ class MyGui():
             self.__plansza_n=sx
             self.__plansza_m=sy
             self.__plansza_mina=smina
+
             print(self.__plansza_n, self.__plansza_m, self.__plansza_mina)
+            #plansza
+
+            self.__text='Start!'
         pass
 
     def Menu(self):
-        n = StringVar()
-        m = StringVar()
-        mina = StringVar()
+        str_e1=tk.StringVar(value='')
+        str_e2=tk.StringVar(value='')
+        str_e3=tk.StringVar(value='')
 
-        label1 = Label(self.__okno, text = 'rozmiar:')
-        label2 = Label(self.__okno, text = 'miny:')
-        entry1 = Entry(self.__okno, textvariable = n)#, width = 25
-        entry2 = Entry(self.__okno, textvariable = m)#, width = 25
-        entry3 = Entry(self.__okno, textvariable = mina)#, width = 25
-        button1 = Button(self.__okno, text = 'Start', command = lambda: self.Start(entry1.get(), entry2.get(), entry3.get()))
-        button2 = Button(self.__okno, text = 'exit', command = self.__okno.destroy)
+        label1 = tk.Label(self.__okno, text = 'rozmiar:')
+        label2 = tk.Label(self.__okno, text = 'miny:')
+        label3 = tk.Label(self.__okno, textvariable=self.__text)
+
+        entry1 = tk.Entry(self.__okno, textvariable=str_e1)
+        entry2 = tk.Entry(self.__okno, textvariable=str_e2)
+        entry3 = tk.Entry(self.__okno, textvariable=str_e3)
+
+        button1 = tk.Button(self.__okno, text = 'Start', command = lambda: self.StartTry(entry1.get(), entry2.get(), entry3.get()))
+        button2 = tk.Button(self.__okno, text = 'Exit', command = self.__okno.destroy)
 
         label1.grid(row=0, column=0)
         entry1.grid(row=0, column=1)
-        entry2.grid(row=0, column=2)
-        label2.grid(row=1, column=0)
-        entry3.grid(row=1, column=1)
-        button1.grid(row=2, column=0)
-        button2.grid(row=2, column=1)
+        entry2.grid(row=1, column=1)
+        label2.grid(row=2, column=0)
+        entry3.grid(row=2, column=1)
+        button1.grid(row=3, column=0)
+        button2.grid(row=3, column=1)
+        label3.grid(row=4, column=0)
+
+
+def gui2():
+    import tkinter as tk
+
+    class Test():
+        def __init__(self):
+            self.root = tk.Tk()
+            self.text = tk.StringVar()
+            self.text.set("Test")
+            self.label = tk.Label(self.root, textvariable=self.text)
+
+            self.button = tk.Button(self.root,
+                                    text="Click to change text below",
+                                    command=self.changeText)
+            self.button.pack()
+            self.label.pack()
+            self.root.mainloop()
+
+        def changeText(self):
+            self.text.set("Text updated")        
+
+    app=Test()
 
 
--- Saper/Saper.py
+++ Saper/Saper.py
@@ -1,10 +1,105 @@
-from tkinter import *
-from Gui import *
+import tkinter as tk
+#import Gui as mygui
 
-okno=Tk()
+class MyGui():
+    def __init__(self, okno):
+        self.__okno = okno
+        self.__okno.title('Saper')
+        self.__plansza_n = 8      #default n = 8
+        self.__plansza_m = 8      #default m = 8
+        self.__plansza_mina = 12  #default mines = 12
+        self.__play=None
 
-gui=MyGui(okno)
+        self.__text = tk.StringVar()#######
+        self.__text.set("Wpisz wartości")############
 
-gui.Menu()
+    def __str__(self):
+        return 'MyGui'
 
-okno.mainloop()
+    def StartTry(self, sx=8, sy=8, smina=12):
+        try:
+            sn=int(sx)
+            sm=int(sy)
+            smi=int(smina)
+
+            if(sn<2 or sn>15):
+                self.__text.set('Wpisz wartość rozmiaru od 2 do 15')
+                return
+            if(sm<2 or sm>15):
+                self.__text.set('Wpisz wartość rozmiaru od 2 do 15')
+                return
+            if(smi<1 or smi>sm*sn-1):
+                self.__text.set('Wpisz wartość min od 1 do ' +  str(sn*sm))
+                return
+        except ValueError as e:
+            self.__text.set('Wpisz liczby!')
+        except Exception as e:
+            print('Błąd\n')
+            print(e.args)
+            raise
+        else:
+            self.__plansza_n=sx
+            self.__plansza_m=sy
+            self.__plansza_mina=smina
+
+            print(self.__plansza_n, self.__plansza_m, self.__plansza_mina)
+            #plansza
+
+            self.__text.set("Start!")
+        pass
+
+    def Menu(self):
+        str_e1=tk.StringVar(value='')
+        str_e2=tk.StringVar(value='')
+        str_e3=tk.StringVar(value='')
+
+        label1 = tk.Label(self.__okno, text = 'rozmiar:')
+        label2 = tk.Label(self.__okno, text = 'miny:')
+        label3 = tk.Label(self.__okno, textvariable=self.__text)
+
+        entry1 = tk.Entry(self.__okno, textvariable=str_e1)
+        entry2 = tk.Entry(self.__okno, textvariable=str_e2)
+        entry3 = tk.Entry(self.__okno, textvariable=str_e3)
+
+        button1 = tk.Button(self.__okno, text = 'Start', command = lambda: self.StartTry(entry1.get(), entry2.get(), entry3.get()))
+        button2 = tk.Button(self.__okno, text = 'Exit', command = self.__okno.destroy)
+
+        label1.grid(row=0, column=0)
+        entry1.grid(row=0, column=1)
+        entry2.grid(row=1, column=1)
+        label2.grid(row=2, column=0)
+        entry3.grid(row=2, column=1)
+        button1.grid(row=3, column=0)
+        button2.grid(row=3, column=1)
+        label3.grid(row=4, column=0)
+
+class Test():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.text = tk.StringVar()
+        self.text.set("Test")
+        self.label = tk.Label(self.root, textvariable=self.text)
+
+        self.button = tk.Button(self.root,
+                                text="Click to change text below",
+                                 command=self.changeText)
+        self.button.pack()
+        self.label.pack()
+        self.root.mainloop()
+
+    def changeText(self):
+        self.text.set("Text updated")        
+
+def main():
+
+    okno=tk.Tk()
+    gui=MyGui(okno)
+    gui.Menu()
+
+
+    okno.mainloop()
+
+    #Test()######
+
+if __name__ == '__main__':
+    main()
